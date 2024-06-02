ef0a5ce62fc27f91e59e33bc3d8833d31880608e
Angadbir Singh <angadbir0987@gmail.com>
2024-06-02 17:55:26 +0530

    latest commit 2 june

--- src/main/java/com/example/clothing/DAO/CartRepository.java
+++ src/main/java/com/example/clothing/DAO/CartRepository.java
@@ -16,7 +16,7 @@ import jakarta.transaction.Transactional;
 @Repository
 public interface CartRepository extends CrudRepository<Cart, Integer> {
 
-    @Query(value = "SELECT clothing_items.id,item_color,item_name,clothing_items.price,SUM(cart.quantity),cart.id AS 'Total Quantity',cart.item_id FROM cart INNER JOIN clothing_items ON clothing_items.id=cart.item_id WHERE customer_id=:customerid GROUP BY item_id", nativeQuery = true)
+    @Query(value = "SELECT clothing_items.id,item_color,item_name,clothing_items.price,SUM(cart.quantity),cart.id,cart.item_id FROM cart INNER JOIN clothing_items ON clothing_items.id=cart.item_id WHERE customer_id=:customerid GROUP BY item_id", nativeQuery = true)
     public List<Object[]> getCartItems(@Param("customerid") String customer_id);
 
     @Transactional
@@ -30,7 +30,7 @@ public interface CartRepository extends CrudRepository<Cart, Integer> {
     @Query(value = "select * from cart where customer_id=:customerid && item_id=:itemid", nativeQuery = true)
     public Cart getByCustomerIdAndItemid(@Param("customerid") String customerid, @Param("itemid") int quan);
 
-    @Query(value = "select item_id from cart where customer_id=:customerid",nativeQuery = true)
+    @Query(value = "select item_id from cart where customer_id=:customerid", nativeQuery = true)
     public List<Integer> getAllClothingItemsIdsFromCart(@Param("customerid") String customerId);
 
     @Query(value = "select item_id,quantity from cart where customer_id=:customerid", nativeQuery = true)
